e6a55a78d4f7c5b0829f5fd1f38f78dd7ae77f7c
Francesca <bambozzif@gmail.com>
2022-09-08 20:36:39 +0200

    Updates

---
+++ precache-manifest.2d7b8a027eee21ed0c7fe56cac154a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc0aa37bc36047308012",
+    "url": "/Booking_App/static/css/main.af69f3e9.chunk.css"
+  },
+  {
+    "revision": "bc0aa37bc36047308012",
+    "url": "/Booking_App/static/js/main.a55cbe37.chunk.js"
+  },
+  {
+    "revision": "09ade156f128508e2a7b",
+    "url": "/Booking_App/static/js/runtime~main.9a45c0d6.js"
+  },
+  {
+    "revision": "a68ee97f505489edb6f3",
+    "url": "/Booking_App/static/js/2.f65867d9.chunk.js"
+  },
+  {
+    "revision": "c57939335d24d598fbba5486015d9684",
+    "url": "/Booking_App/static/media/body.c5793933.jpg"
+  },
+  {
+    "revision": "9ee80ef97d34b4aae50fbfee7f484eaa",
+    "url": "/Booking_App/index.html"
+  }
+];
